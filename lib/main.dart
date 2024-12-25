724fd41b621df14748e480adb854dca4dbda4078
Діма Шимко <dimashimko7@gmail.com>
2024-12-25 16:22:59 +0200

    added localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'package:path_finding/routes/app_router.dart';
 
+import 'generated/l10n.dart';
 import 'pages/home_page/blocs/home_bloc/home_bloc.dart';
 import 'repos/data_repo/data_repository.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     const QssInspectorApp(),
   );
@@ -35,9 +38,15 @@ class QssInspectorApp extends StatelessWidget {
         ],
         child: MaterialApp.router(
           title: 'QSS inspectors',
-          // theme: AppThemes.light(),
           routerConfig: router,
-          // builder: BotToastInit(),
+          // locale: state.locale,
+          supportedLocales: S.delegate.supportedLocales,
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
           debugShowCheckedModeBanner: false,
         ),
       ),
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
+import '../../generated/l10n.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    S tr = S.of(context);
     return Scaffold(
       body: Center(
-        child: Text('Home page'),
+        child: Text(tr.home_page),
       ),
     );
   }
